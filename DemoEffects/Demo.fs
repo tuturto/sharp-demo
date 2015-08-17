d147a0b6b7c3e6a6660b56535cb4156a1eefe4bb
Tuukka Turto <tuukka.turto@oktaeder.net>
2015-08-17 22:12:23 +0300

    Initial import

---
+++ DemoEffects/Demo.fs
@@ -0,0 +1,28 @@
+﻿module Demo
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Content
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Input
+
+type Game () as this =
+    inherit Microsoft.Xna.Framework.Game()
+ 
+    do this.Content.RootDirectory <- "Content"
+    let graphics = new GraphicsDeviceManager(this)
+    let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+ 
+    override this.Initialize() =
+        do spriteBatch <- new SpriteBatch(this.GraphicsDevice)
+        do base.Initialize()
+        ()
+ 
+    override this.LoadContent() =
+        ()
+ 
+    override this.Update (gameTime) =
+        ()
+ 
+    override this.Draw (gameTime) =
+        do this.GraphicsDevice.Clear Color.CornflowerBlue
+        ()
---
+++ DemoEffects/Program.fs
@@ -0,0 +1,7 @@
+﻿open Demo
+
+[<EntryPoint>]
+let main argv = 
+    use g = new Game()
+    g.Run()
+    0 // return an integer exit code
