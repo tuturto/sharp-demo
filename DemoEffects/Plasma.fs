867920dcc1dcc442d01263dfde6ed999acfae954
Tuukka Turto <tuukka.turto@oktaeder.net>
2015-08-19 07:20:44 +0300

    Clean up plasma implementation

--- DemoEffects/Plasma.fs
+++ DemoEffects/Plasma.fs
@@ -35,7 +35,7 @@ let pointList =
     |> Seq.concat
     |> List.ofSeq
 
-let plasmaOne tick point =
+let plasmaIntensity tick point =
     let x' = (float)point.x / width' * (8.0 * sin (tick / 12.0))
     let y' = (float)point.y / height' * (8.0 * cos (tick / 20.0))
     let cx = x' + 0.5 * sin ( tick / 5.0 )
@@ -45,15 +45,16 @@ let plasmaOne tick point =
           sin ( 2.0 * ( x' * sin ( tick / 2.0 ) + y' * cos ( tick / 3.0 )) + tick ) +
           sin ( sqrt ( 10.0 * ( cx*cx + cy*cy ) ) + 0.5 + tick); }
 
-let colour ( v : PlasmaPoint ) =
+let colourOfV ( v : PlasmaPoint ) =
     { location = v.location; 
       colour = { r = (int)(( sin ( v.v * (float)MathHelper.Pi ) + 1.0 ) * 128.0 );
                  b = (int)(( cos ( v.v * (float)MathHelper.Pi ) + 1.0 ) * 128.0 );
                  g = (int)((( sin ( v.v * (float)MathHelper.Pi ) + 1.0 ) * 64.0 ) + (( cos ( v.v * (float)MathHelper.Pi ) + 1.0 ) * 64.0 )); } }
 
+let colour tick = plasmaIntensity tick >> colourOfV
+
 let plasma tick =
-    let vList = List.map (plasmaOne tick) pointList
-    List.map colour vList
+    List.map (colour tick) pointList
 
 let drawPoint (spriteBatch : SpriteBatch) pixel point =
     spriteBatch.Draw(pixel, Rectangle(point.location.x * pixelSize, point.location.y * pixelSize, pixelSize, pixelSize), Color(point.colour.r, point.colour.g, point.colour.b))
